483aa965095c1051f096ee80019419645c8998c6
john dave pacinio <john-dave.pacinio@student.passerellesnumeriques.org>
2021-01-27 00:00:33 +0800

    init

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require ('express');
-
+const express = require('express')
 const app = express();
 
 app.use(express.static('./dist/angular-specialization'));
-app.get('/*', (req,res) => {
-	res.sendFile('index.html',
-	 {root:'./dist/angular-specialization/'})
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: './dist/angular-specialization/' })
 });
-app.listen(process.env.PORT ||8080)
+
+
+app.listen(process.env.PORT || 8080)
